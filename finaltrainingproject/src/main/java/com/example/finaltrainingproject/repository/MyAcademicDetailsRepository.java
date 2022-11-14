66c3c0117228a21aa0eb5c84d182927233e57ab6
ashwinmphilip <84561862+ashwinmphilip@users.noreply.github.com>
2022-11-14 11:42:20 +0530

    Delete finaltrainingproject directory

--- finaltrainingproject/src/main/java/com/example/finaltrainingproject/repository/MyAcademicDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.finaltrainingproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.finaltrainingproject.entity.AcademicDetails;
-
-public interface MyAcademicDetailsRepository extends JpaRepository<AcademicDetails, Long>{
-
-}
--- finaltrainingproject/src/main/java/com/example/finaltrainingproject/repository/MyStudentCourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.finaltrainingproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.finaltrainingproject.entity.StudentCourse;
-
-public interface MyStudentCourseRepository extends JpaRepository<StudentCourse, Long>{
-
-}
